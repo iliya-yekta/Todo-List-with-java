efc890b759caacf680b9ff112e20afe350a39f11
Hikaru <iliyayekta384@gmail.com>
2026-02-11 17:10:36 +0330

    Add main files

---
+++ Main/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main extends Task{
+    Main(String name) {
+        super(name);
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Task> tasks = new ArrayList<>();
+    static String input = "";
+
+    static void main() {
+
+        // Store input
+        short choice;
+        do {
+            IO.println("-=-=-=-=-=-=-=-=- WELCOME -=-=-=-=-=-=-=-=-");
+            IO.println("""
+                    1 -> Add a task         2 -> Change a task\s
+                    3 -> Remove a task      4 -> Show a task\s
+                    5 -> Show all tasks     6 -> Mark a task complete\s
+                    7 -> Exit
+                    """);
+            choice = scanner.nextShort();
+            scanner.nextLine();
+
+            // Examine input
+            switch (choice) {
+                case 1 -> {
+                    // Add task
+                    IO.print("Write task name to add: ");
+                    tasks.add(new Task(scanner.nextLine()));
+                }
+                case 2 -> {
+                    // Change a stat
+                    if (is_empty()) continue;
+                    IO.print("Enter name of the tasks to change: ");
+                    if (!is_exist(tasks, (input = scanner.nextLine()))) continue;
+                    change_task(tasks, input);
+                }
+                case 3 -> {
+                    // Remove a task
+                    if (is_empty()) continue;
+                    IO.print("Enter the task name to remove: ");
+                    if (!is_exist(tasks, (input = scanner.nextLine()))) continue;
+                    remove_task(tasks, input);
+                }
+                case 4 -> {
+                    // Demonstrate a task
+                    if (is_empty()) continue;
+                    IO.print("Enter the task name to show: ");
+                    if (!is_exist(tasks, (input = scanner.nextLine()))) continue;
+                    for (Task task: tasks) if (input.equals(task.name)) {
+                        System.out.println(task);
+                    }
+                }
+                case 5 -> {
+                    // Demonstrate all task
+                    if (is_empty()) continue;
+                    for (Task task: tasks) IO.println(task + "\n");
+                }
+
+                case 6 -> {
+                    // Complete a task
+                    if (is_empty()) continue;
+                    IO.print("Write name of the task to mark as complete: ");
+                    if (!is_exist(tasks, (input = scanner.nextLine()))) continue;
+                    mark_as_completed(tasks, input);
+                }
+                default -> IO.println("Invalid choice. Try again...");
+            }
+        } while (choice != 7);
+        scanner.close();
+    }
+
+    static boolean is_empty() {
+        if (tasks.isEmpty()) IO.println("Task list is empy. Add a task... ");
+        return true;
+    }
+}
+
---
+++ Main/src/Task.java
@@ -0,0 +1,61 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Task {
+
+    String name;
+    HashMap<String, String> time = new HashMap<>();
+    boolean is_completed = false;
+
+
+    Task(String name) {
+        this.name = name;
+        this.time.put("Created", LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+    }
+
+    // Complete task
+    public static void mark_as_completed(ArrayList<Task> tasks, String input) {
+        for (Task task: tasks) if (task.name.equals(input)) task.is_completed = true;
+    }
+
+    // Change a task
+    public static void change_task(ArrayList<Task> task_list, String input) {
+        Scanner change_input = new Scanner(System.in);
+
+        for (Task task : task_list)
+            if (task.name.equals(input)) {
+                IO.print("Enter name to change: ");
+                task.name = change_input.nextLine();
+            }
+    }
+
+    // Remove a task
+    public static void remove_task(ArrayList<Task> task_list, String input) {
+        task_list.removeIf(task -> input.equals(task.name));
+    }
+
+    // Stats of a task
+    public String toString() {
+        return "Stats: \nname -> " + this.name + "\nTime created -> " + this.time.get("Created") + "\nCondition -> " + (!this.is_completed ? "not completed"  : "completed");
+
+    }
+
+    // Check existing task name
+    public static boolean is_exist(ArrayList<Task> task_list, String input) {
+        boolean exist = false;
+
+        for (Task task: task_list)
+            if (task.name.equals(input)) {
+                exist = true;
+                break;
+            }
+        if (!exist) {
+            IO.println("This name doesn't exist...");
+            return false;
+        }
+        return true;
+    }
+}
